ac7a6f9b365c160d2992af2339c8950374589ce0
maxim <maxim@filjus.ru>
2023-02-02 17:23:39 +0500

    пользователи без токена считаются незарегистрированными

--- src/tgbot/filters/user.py
+++ src/tgbot/filters/user.py
@@ -8,7 +8,8 @@ from tgbot.services.repository import Repo
 
 class RegisteredUserFilter(BaseFilter):
     async def __call__(self, message: Message, event_from_user: User, repo: Repo) -> Union[bool, Dict[str, Any]]:
-        return repo.has_user(event_from_user.id) == self.is_user
+        return (repo.has_user(event_from_user.id) and
+                repo.get_user(event_from_user.id).jwtoken is not None) == self.is_user
 
     def __init__(self, is_user: bool):
         self.is_user = is_user
