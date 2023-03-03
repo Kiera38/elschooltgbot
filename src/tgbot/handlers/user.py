ea9b3a4b19f388a60dd2d07b6a84ec872d929f39
maxim <maxim@filjus.ru>
2023-03-03 20:30:05 +0500

    ДЛя функций обработчиков добавлена документация

--- src/tgbot/handlers/user.py
+++ src/tgbot/handlers/user.py
@@ -20,11 +20,15 @@ grades_router = Router()
 
 @router.message(CommandFilter(True))
 async def text_is_command(message: Message):
+    """В случае, если пользователь написал команду вместо того,
+    что его попросили, будет вызываться эта функция.
+    """
     await message.answer('по моему ты написал одну из моих команд. Тебя что попросили написать?')
 
 
 @router.message(StateFilter(ParamsGetter.GET_LOGIN), CommandFilter())
 async def get_user_login(m: Message, state: FSMContext):
+    """Когда пользователь при регистрации написал логин, вызывается эта функция."""
     await state.update_data(login=m.text)
     await m.delete()
     await m.answer('логин получил, теперь напиши пароль')
@@ -33,6 +37,7 @@ async def get_user_login(m: Message, state: FSMContext):
 
 @router.message(StateFilter(ParamsGetter.GET_PASSWORD), CommandFilter())
 async def get_user_password(m: Message, repo: Repo, state: FSMContext):
+    """Когда пользователь при регистрации написал пароль, вызывается эта функция."""
     await m.delete()
     data = await state.get_data()
     await m.answer('проверка введённых данных, попытка регистрации')
@@ -49,6 +54,7 @@ async def get_user_password(m: Message, repo: Repo, state: FSMContext):
 
 @router.message(StateFilter(ParamsGetter.GET_QUARTER), CommandFilter())
 async def get_user_quarter(m: Message, repo: Repo, state: FSMContext):
+    """Когда пользователь при регистрации указал четверть, вызывается эта функция."""
     data = await state.get_data()
     if m.text in data['quarters']:
         quarter = m.text
@@ -64,12 +70,14 @@ async def get_user_quarter(m: Message, repo: Repo, state: FSMContext):
 
 @router.message(Command('register'))
 async def register(m: Message, state: FSMContext):
+    """Когда пользователь захотел зарегистрироваться."""
     await state.set_state(ParamsGetter.GET_LOGIN)
     await m.answer('сейчас нужно ввести свои данные, сначала логин')
 
 
 @router.message(Text('регистрация'))
 async def keyboard_register(m: Message, state: FSMContext, repo: Repo):
+    """Показывает страницу регистрации клавиатуры."""
     await state.set_state(Page.REGISTER)
     await state.update_data(message=m)
     await m.answer('выбери действие', reply_markup=register_keyboard(
@@ -78,6 +86,7 @@ async def keyboard_register(m: Message, state: FSMContext, repo: Repo):
 
 @router.callback_query(Text('register'), StateFilter(Page.REGISTER))
 async def keybard_register_user(query: CallbackQuery, state: FSMContext):
+    """Регистрация из клавиатуры."""
     await query.message.edit_reply_markup(cancel_keyboard())
     data = await state.get_data()
     await register(data['message'], state)
@@ -86,11 +95,13 @@ async def keybard_register_user(query: CallbackQuery, state: FSMContext):
 
 @registered_router.message(Text('оценки'))
 async def grades(m: Message, state: FSMContext):
+    """Показывает страницу оценок клавиатуры"""
     await state.set_state(Page.GRADES)
     await m.answer('выбери действие', reply_markup=grades_keyboard())
 
 
-async def grades_query(query: CallbackQuery, repo: Repo, state: FSMContext, next_state, answer_text: str, show_all):
+async def grades_query(query: CallbackQuery, repo: Repo, state: FSMContext, next_state, answer_text: str):
+    """Вспомогательная функция для обработки действий со страницы оценок."""
     user = repo.get_user(query.from_user.id)
     if user.cached_grades:
         if user.quarter not in user.cached_grades:
@@ -110,21 +121,25 @@ async def grades_query(query: CallbackQuery, repo: Repo, state: FSMContext, next
 
 @registered_router.callback_query(Text('get_grades'), StateFilter(Page.GRADES))
 async def get_grades_query(query: CallbackQuery, repo: Repo, state: FSMContext):
-    await grades_query(query, repo, state, Page.GET_GRADES, 'какие оценки получить', True)
+    """Когда пользователь выбрал показать оценки на станице оценок клавиатуры."""
+    await grades_query(query, repo, state, Page.GET_GRADES, 'какие оценки показать')
 
 
 @registered_router.callback_query(Text('fix_grades'), StateFilter(Page.GRADES))
 async def fix_grades_query(query: CallbackQuery, repo: Repo, state: FSMContext):
-    await grades_query(query, repo, state, Page.FIX_GRADES, 'какие оценки исправить', True)
+    """Когда пользователь выбрал исправить оценки на странице оценок клавиатуры."""
+    await grades_query(query, repo, state, Page.FIX_GRADES, 'какие оценки исправить')
 
 
 @grades_router.message(Command('get_grades'))
 async def get_grades(m: Message, grades):
+    """Обработчик для команды показа оценок, но также функция для показа оценок."""
     await m.answer(show_grades(grades), reply_markup=main_keyboard())
 
 
 @grades_router.callback_query(Text('all_grades'), StateFilter(Page.GET_GRADES))
 async def get_all_grades(query: CallbackQuery, grades: dict, state: FSMContext):
+    """Когда пользователь выбрал показать все оценки на клавиатуре."""
     await get_grades(query.message, grades)
     await state.clear()
     await query.answer()
@@ -132,12 +147,14 @@ async def get_all_grades(query: CallbackQuery, grades: dict, state: FSMContext):
 
 @grades_router.message(StateFilter(Page.GET_GRADES))
 async def keyboard_get_grades(m: Message, grades: dict, state: FSMContext):
+    """Когда пользователь выбрал показать оценки по конкретному уроку."""
     await grades_one_lesson(m, grades)
     await state.clear()
 
 
 @grades_router.message(StateFilter(Page.FIX_GRADES))
 async def keyboard_fix_grades(m: Message, grades: dict, state: FSMContext):
+    """Когда пользователь выбрал исправить оценки по конкретному уроку."""
     if m.text == 'все':
         await fix_grades(m, grades)
     else:
@@ -155,6 +172,7 @@ async def keyboard_fix_grades(m: Message, grades: dict, state: FSMContext):
 
 @registered_router.message(Command('clear_cache'))
 async def clear_cache(m: Message, repo: Repo):
+    """Очистка сохранённых оценок."""
     user = repo.get_user(m.from_user.id)
     user.cached_grades = None
     await m.answer('очистил сохранённые оценки')
@@ -162,6 +180,7 @@ async def clear_cache(m: Message, repo: Repo):
 
 @registered_router.message(Command('update_cache'))
 async def update_cache(m: Message, repo: Repo):
+    """Обновление сохранённых оценок."""
     await clear_cache(m, repo)
     await m.answer('обновляю сохранённые оценки')
     grades, time = await repo.get_grades(repo.get_user(m.from_user.id))
@@ -170,6 +189,7 @@ async def update_cache(m: Message, repo: Repo):
 
 @registered_router.callback_query(Text('back_grades'))
 async def back_grades(query: CallbackQuery, state: FSMContext):
+    """Обработка кнопки назад для возврата к странице оценок."""
     await state.set_state(Page.GRADES)
     await query.message.edit_reply_markup(grades_keyboard(True))
     data = await state.get_data()
@@ -181,6 +201,7 @@ async def back_grades(query: CallbackQuery, state: FSMContext):
 
 @registered_router.callback_query(Text('back_main'))
 async def back_main(query: CallbackQuery, state: FSMContext):
+    """Обработка кнопки назад для возврата к основной странице."""
     await state.clear()
     await query.message.answer('основное меню', reply_markup=main_keyboard())
     await query.message.edit_reply_markup(None)
@@ -189,17 +210,20 @@ async def back_main(query: CallbackQuery, state: FSMContext):
 
 @router.message(Command('start'))
 async def user_start(m: Message):
+    """При старте бота показать приветственное сообщение."""
     await m.reply("привет, я могу присылать тебе оценки из электронного журнала elschool и подсказывать как их испавить\n"
                   "чтобы узнать как пользоваться используй /help", reply_markup=main_keyboard())
 
 
 @grades_router.message(Command('fix_grades'))
 async def fix_grades(m: Message, grades):
+    """Обработка команды исправления оценок и функция для показа сообщения исправления оценок."""
     await m.answer(show_fix_grades(grades), reply_markup=main_keyboard())
 
 
 @grades_router.callback_query(Text('all_grades'), StateFilter(Page.FIX_GRADES))
 async def fix_all_grades(query: CallbackQuery, grades: dict, state: FSMContext):
+    """Обработка кнопки исправления всех оценок."""
     await fix_grades(query.message, grades)
     await state.clear()
     await query.answer()
@@ -208,6 +232,7 @@ async def fix_all_grades(query: CallbackQuery, grades: dict, state: FSMContext):
 @router.message(Command('version'))
 @router.message(Text('версия'))
 async def version(m: Message):
+    """Показать версию и список изменений."""
     await m.answer("""моя версия: 2.5.10.dev2 (обновление 3.1)
 список изменений:
 Исправление ошибок
@@ -216,6 +241,7 @@ async def version(m: Message):
 
 @router.message(Command('version_comments'))
 async def version_comments(m: Message):
+    """Подробно показать список изменений."""
     await m.answer("""моя версия: 2.5.10.dev2 (обновление 3.1)
 
 Исправление ошибок (ну как исправление. Скорее мойка кота.)
@@ -225,6 +251,7 @@ async def version_comments(m: Message):
 
 @router.message(Command('help'))
 async def help(m: Message):
+    """Показать помощь."""
     await m.answer("""я могу присылать тебе оценки из электронного журнала elschool и подсказывать как их испавить
 
 команды:
@@ -244,11 +271,12 @@ async def help(m: Message):
 
 Для получения оценок нужно обязательно указать свои данные с помощью /register или с помощью пункта в меню регистрация. Потребуется логин и пароль. Доступа к этим данным у разработчика нет. Всё это описано в политике конфиденциальности. 
 
-Также, если просто написать название урока можно получить подробную информацию по каждой оценке""")
+Также, если просто написать название урока можно получить подробную информацию по каждой оценке""") #TODO добавить помощь по кнопкам.
 
 
 @registered_router.message(StateFilter(Change.QUARTER), CommandFilter())
 async def quarter_changed(m: Message, state: FSMContext, repo: Repo):
+    """Когда пользователь выбрал четверть после того, как захотел её изменить."""
     user = repo.get_user(m.from_user.id)
     quarters = user.cached_grades
     if m.text in quarters:
@@ -263,6 +291,7 @@ async def quarter_changed(m: Message, state: FSMContext, repo: Repo):
 
 
 async def change_quarter(m: Message, user: User, state: FSMContext, repo: Repo):
+    """Пользователь захотел изменить четверть."""
     await state.set_state(Change.QUARTER)
     if user.cached_grades:
         grades = user.cached_grades
@@ -276,12 +305,14 @@ async def change_quarter(m: Message, user: User, state: FSMContext, repo: Repo):
 
 @registered_router.message(Command('change_quarter'), CommandFilter())
 async def change_quarter_command(m: Message, repo: Repo, state: FSMContext):
+    """Пользователь захотел изменить четверть из клавиатуры."""
     user = repo.get_user(m.from_user.id)
     await change_quarter(m, user, state, repo)
 
 
 @registered_router.callback_query(Text('change_quarter'))
 async def change_quarter(query: CallbackQuery, state: FSMContext, repo: Repo):
+    """Пользователь захотел изменить четверть с помощью команды."""
     user = repo.get_user(query.from_user.id)
     await change_quarter(query.message, user, state, repo)
     await query.answer()
@@ -290,6 +321,7 @@ async def change_quarter(query: CallbackQuery, state: FSMContext, repo: Repo):
 @router.message(Command('privacy_policy'))
 @router.message(Text('политика конфиденциальности'))
 async def privacy_policy(m: Message):
+    """Показать политику конфиденциальности."""
     await m.answer('это что-то похожее на политику конфиденциальности. '
                    'Если кто-то напишет нормальную политику конфиденциальности, я её обновлю.')
     await m.answer("Для получения оценок бот просит логин и пароль от журнала elschool. "
@@ -299,12 +331,17 @@ async def privacy_policy(m: Message):
 
 @registered_router.message(Command('reregister'))
 async def reregister(m: Message, state: FSMContext):
+    """Пользователь захотел изменить данные.
+
+    Это обработчик для команды и вспомогательная функция при использовании клавиатуры.
+    """
     await m.answer('сейчас можно изменить свой логин и пароль, сначала введи новый логин')
     await state.set_state(Change.LOGIN)
 
 
 @registered_router.message(StateFilter(Change.LOGIN), CommandFilter())
 async def change_login(m: Message, state: FSMContext):
+    """После ввода логина при изменении данных аккаунта."""
     await state.update_data(login=m.text)
     await state.set_state(Change.PASSWORD)
     await m.answer('хорошо, теперь напиши пароль')
@@ -313,6 +350,7 @@ async def change_login(m: Message, state: FSMContext):
 
 @registered_router.message(StateFilter(Change.PASSWORD), CommandFilter())
 async def change_password(m: Message, state: FSMContext, repo: Repo):
+    """После ввода пароля при изменении данных аккаунта."""
     data = await state.get_data()
     login = data['login']
     password = m.text
@@ -327,6 +365,7 @@ async def change_password(m: Message, state: FSMContext, repo: Repo):
 
 @registered_router.callback_query(Text('change_data'), StateFilter(Page.REGISTER))
 async def change_data(query: CallbackQuery, state: FSMContext):
+    """Пользователь захотел изменить данные с помощью кнопки на клавиатуре."""
     await query.message.edit_reply_markup(cancel_keyboard())
     data = await state.get_data()
     await reregister(data['message'], state)
@@ -334,6 +373,11 @@ async def change_data(query: CallbackQuery, state: FSMContext):
 
 @registered_router.message(Command('unregister'))
 async def unregister(m: Message, repo: Repo):
+    """Пользователь захотел изменить данные.
+
+    Как всегда, это обработчик для команды и вспомогательная
+    функция при обработке события нажатия кнопки.
+    """
     try:
         repo.remove_user(m.from_user.id)
     except KeyError:
@@ -344,6 +388,7 @@ async def unregister(m: Message, repo: Repo):
 
 @registered_router.callback_query(Text('remove_data'), StateFilter(Page.REGISTER))
 async def remove_data(query: CallbackQuery, repo: Repo, state: FSMContext):
+    """Пользователь захотел изменить данные с помощью кнопки на клавиатуре."""
     await query.message.edit_reply_markup(cancel_keyboard())
     data = await state.get_data()
     await unregister(data['message'], repo)
@@ -352,11 +397,15 @@ async def remove_data(query: CallbackQuery, repo: Repo, state: FSMContext):
 
 @router.message(RegisteredUserFilter(False))
 async def no_user(m: Message):
+    """Пользователь не зарегистрирован,
+    но захотел использовать функцию для зарегистрированных пользователей\
+    """
     await m.answer('тебя нет в списке пользователей, попробуй зарегистрироваться')
 
 
 @grades_router.message()
 async def grades_one_lesson(m: Message, grades):
+    """Показать оценки для одного урока."""
     if m.text == 'все':
         await get_grades(m, grades)
     else:
@@ -372,6 +421,7 @@ async def grades_one_lesson(m: Message, grades):
 
 
 def register_user(dp: Dispatcher):
+    """Настройка роутеров."""
     dp.include_router(router)
     registered_router.message.filter(RegisteredUserFilter(True))
     router.include_router(registered_router)
